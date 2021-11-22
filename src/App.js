64f256cf4738bdf1bacd0c11c3498f7938f0f066
vijay kumar vemana <vijaykumarvemana18@gmail.com>
2021-11-22 23:14:06 +0100

    Adds sidevar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
 import Topbar from './components/topbar/Topbar';
-
+import Sidebar from './components/sidebar/Sidebar';
 function App() {
   return (
+   <div>
     <Topbar />
+    <div className="container">
+    <Sidebar />
+other pages
+    </div>
+    </div>
   );
 }
 
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,64 @@
+import './Sidebar.css'
+import { AttachMoney, BarChart, DynamicFeed, LineStyle, MailOutline, PermIdentity, Storefront, Timeline, TrendingUp } from "@material-ui/icons"
+
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <div className="sidebar-Wrapper">
+                <div className="sidebar-Menu">
+                    <h3 className="sidebar-Title">Dashboard</h3>
+                    <ul className="sidebar-List">
+                        <li className="sidebar-Item active">
+                            <LineStyle className="sidebar-Icon" />
+                            Home
+                        </li>
+                        <li className="sidebar-Item">
+                            <PermIdentity className="sidebar-Icon" />
+                            Users 
+                        </li>
+                        <li className="sidebar-Item">
+                            <Storefront className="sidebar-Icon" />
+                            Products
+                        </li>
+                    <li className="sidebar-Item">
+                        <AttachMoney className="sidebar-Icon" />
+                        Transactions 
+                    </li>
+                    <li className="sidebar-Item">
+                        <TrendingUp className="sidebar-Icon" />
+                        Sales
+                    </li>
+                    
+
+                    <li className="sidebar-Item">
+                        <BarChart className="sidebar-Icon" />
+                        Reports
+                    </li>
+                    </ul>
+                </div>
+                <div className="sidebar-Menu">
+                    <h3 className="sidebar-Title">Notifications</h3>
+                    <ul className="sidebar-List">
+                        <li className="sidebar-Item">
+                            <MailOutline className="sidebar-Icon" />
+                            Mail 
+                        </li>
+                        <li className="sidebar-Item">
+                            <DynamicFeed className="sidebar-Icon" />
+                            Feedback 
+                        </li>
+                        <li className="sidebar-Item">
+                            <Timeline className="sidebar-Icon" />
+                            Analytics 
+                        </li>
+                    </ul>
+                </div>
+                </div>
+            </div>
+
+
+    )
+}
+
+export default Sidebar;
