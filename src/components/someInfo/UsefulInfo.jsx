6d44a825dbebea35dbb3c3cb323c89e65deb38fe
vijay kumar vemana <vijaykumarvemana18@gmail.com>
2021-12-17 22:09:40 +0100

    Update UsefulInfo.jsx

--- src/components/someInfo/UsefulInfo.jsx
+++ src/components/someInfo/UsefulInfo.jsx
@@ -9,7 +9,8 @@ const UsefullInfo = () => {
     const transactions = useSelector(state => state.transaction.transactions);
     
     return (
-        <div style={{display:"flex", color:"white"}}>
+        <div className="d-flex text-white">
+        
           <div className="usefull-Item Item1">
             <span className="usefull-Info-Title">Total Products</span>
             
@@ -50,8 +51,8 @@ const UsefullInfo = () => {
             </div>
               
             </div>
-        </div>
-
+     
+</div>
 
 
 
